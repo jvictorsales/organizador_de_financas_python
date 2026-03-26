fe16138362aeccaea7ba3402b7c8d58a35e1b62d
jvictorsales <joaovictorlsales@gmail.com>
2026-03-26 20:11:44 -0300

    Adicionando os @setters

--- transacao.py
+++ transacao.py
@@ -1,26 +1,41 @@
 class Transacao:
     def __init__(self, tipo, valor, descricao, data):
-        self._tipo = tipo
-        self._valor = valor
-        self._descricao = descricao
-        self._data = data
+        self.tipo = tipo
+        self.valor = valor
+        self.descricao = descricao
+        self.data = data
 
     @property
     def tipo(self):
         return self._tipo
-    
+
+    @tipo.setter
+    def tipo(self, tipo):
+        self._tipo = tipo
+
     @property
     def valor(self):
         return self._valor
-    
+
+    @valor.setter
+    def valor(self, valor):
+        self._valor = valor
+
     @property
     def descricao(self):
         return self._descricao
+
+    @descricao.setter
+    def descricao(self, descricao):
+        self._descricao = descricao
 
     @property
     def data(self):
         return self._data
 
+    @data.setter
+    def data(self, data):
+        self._data = data
 
     def __str__(self):
         return f'{self.tipo:<6} | {self.valor:>8.2f} | {self.descricao:<15} | {self.data}'
