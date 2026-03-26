865201d0c358c3c92e50c609a50cbb637669a65d
jvictorsales <joaovictorlsales@gmail.com>
2026-03-26 14:14:40 -0300

    Adicionando @property

--- transacao.py
+++ transacao.py
@@ -1,12 +1,29 @@
 class Transacao:
     def __init__(self, tipo, valor, descricao, data):
-        self.tipo = tipo
-        self.valor = valor
-        self.descricao = descricao
-        self.data = data
+        self._tipo = tipo
+        self._valor = valor
+        self._descricao = descricao
+        self._data = data
+
+    @property
+    def tipo(self):
+        return self._tipo
+    
+    @property
+    def valor(self):
+        return self._valor
+    
+    @property
+    def descricao(self):
+        return self._descricao
+
+    @property
+    def data(self):
+        return self._data
+
 
     def __str__(self):
-        return(f'{self.tipo:<6} | {self.valor:>8.2f} | {self.descricao:<15} | {self.data}')
+        return f'{self.tipo:<6} | {self.valor:>8.2f} | {self.descricao:<15} | {self.data}'
     
     @classmethod
     def from_dict(cls, dados: dict) -> 'Transacao':
